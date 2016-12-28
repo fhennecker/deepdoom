48c0874641ec37df6c242a4a8037002e5b8428a3
Nikita Marchant <nikita.marchant@gmail.com>
2016-12-28 17:13:11 +0100

    optionalize matplotlib

--- src/map_parser.py
+++ src/map_parser.py
@@ -1,6 +1,5 @@
 import json
 import map_parser
-import matplotlib.pyplot as plt
 
 
 def wad_to_walls(string):
@@ -61,6 +60,7 @@ def parse(filename):
 
 
 def plot_map(map, ennemies=None, player_pos=None):
+    import matplotlib.pyplot as plt
 
     walls = map_parser.parse("maps/%s.txt" % map)
     for a, b in walls:
